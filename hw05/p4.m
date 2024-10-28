18cbeea777a6c30f00e0dce932c1ea849c2d87c5
Anant Girdhar <anant.girdhar@proton.me>
2024-10-28 01:38:26 -0400

    Solve HW#5 Problems 4 and 5

---
+++ hw05/p4.m
@@ -0,0 +1,33 @@
+fprintf('Homework 05 Problem 4\n\n')
+% Author: Anant Girdhar
+% Date: 2024-10-28
+
+% Load relevant utilities
+addpath(genpath('../utils/'))
+addpath(genpath('../kalman/'))
+
+set(groot, 'defaultLineLineWidth', 2.0);
+
+% % Load the results from problem 3
+% load('P3_results.mat')
+% true_A_BR_TRIAD_1 = estimated_A_BR_TRIAD_moon;
+% true_A_BR_TRIAD_2 = estimated_A_BR_TRIAD_sun;
+% clearvars estimated_A_BR_TRIAD_moon estimated_A_BR_TRIAD_sun;
+
+fprintf('Part (a)\n')
+% Initialize Kalman filter state
+qp = [0, 0, 0, 1]'
+Pp = deg2rad(5) / sqrt(3) * eye(3)
+
+fprintf('Part (b)\n')
+% Initialize Kalman filter dynamics
+dt = 1;
+omega_meas = [0.6150, 0.0017, -0.5997]' * 1e-3;  % rad/s
+Phi = Phi_calibrated_gyro(omega_meas, dt)
+sigma_v = deg2rad(0.1 / 3600);  % rad/sec
+Q = Q_calibrated_gyro(sigma_v, dt)
+
+fprintf('Part (c)\n')
+% Propagate the measurement one time step
+Y = eye(3);  % process noise "gain" matrix
+[qm, Pm] = propagate_attitude(qp, omega_meas, Pp, Phi, Y, Q, dt)
---
+++ hw05/p5.m
@@ -0,0 +1,36 @@
+fprintf('Homework 05 Problem 5\n\n')
+% Author: Anant Girdhar
+% Date: 2024-10-28
+
+% Load relevant utilities
+addpath(genpath('../utils/'))
+addpath(genpath('../kalman/'))
+
+set(groot, 'defaultLineLineWidth', 2.0);
+
+% Load measureents given in problem
+r1 = [0.5, 0, -sqrt(3)/2]';
+r2 = [0, 0, -1]';
+b1 = [0.7338, 0.1985, 0.6498]';
+b2 = [0.9999, 0.0138, -0.0032]';
+sigma_1 = deg2rad(10) / sqrt(3);  % rad
+sigma_2 = deg2rad(1) / sqrt(3);  % rad
+
+sigmas = [sigma_1; sigma_2];
+r_vectors = [r1; r2];
+b_vectors = [b1; b2];
+
+fprintf('Part (a)\n')
+% Compute quantities for measurement update step
+R = R_isotropic_sensors(sigmas)
+Aqm = quat2DCM(qm)
+hm = h(Aqm, r_vectors)
+Hm = H_calibrated_gyro(Aqm, r_vectors)
+K = kalman_gain(Hm, Pm, R)
+
+fprintf('Part (b) and (c)\n')
+% Perform the measurement update step
+dt = 1;
+dtheta_m = zeros(3, 1);
+y = b_vectors;
+[qp, Pp, dtheta_p] = incorporate_measurement(qm, dtheta_m, y, hm, Hm, Pm, R)
